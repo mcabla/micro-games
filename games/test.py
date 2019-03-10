b124a6727f6eb91918ffa0717609faf0cf675032
Casper H <minecabla@users.noreply.github.com>
2019-03-10 18:06:14 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -3,6 +3,8 @@ import android, time
 droid = android.Android()
 
 while 1:
+	droid.makeToast("test")
+	time.sleep(5)
 	x = droid.getPackageVersionCode("com.mcabla.microbit.game")
 	txt = "app version is: " + str(x)
 	droid.makeToast(txt)
