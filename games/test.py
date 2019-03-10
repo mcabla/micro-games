410902bb717a188fd4a0a54fe3897c2d186a3f76
Casper H <minecabla@users.noreply.github.com>
2019-03-10 18:11:38 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -6,6 +6,7 @@ while 1:
 	droid.makeToast("test")
 	time.sleep(5)
 	x = droid.getPackageVersionCode("com.mcabla.microbit.game")
-	txt = "app version is: " + str(x)
+	y = droid.button_a_was_pressed()
+	txt = "app version is: " + str(x) + " button A was pressed: " + str(y)
 	droid.makeToast(txt)
 	time.sleep(5)
