237525f234bfd828848a2f01cdd2f249ce47d35e
Casper H <minecabla@users.noreply.github.com>
2019-03-10 18:18:38 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -2,11 +2,10 @@ import android, time
 
 droid = android.Android()
 
-droid.makeToast("test")
+droid.makeToast("test2")
 time.sleep(5)
 
 while 1:
-	x = droid.getPackageVersionCode("com.mcabla.microbit.game")
 	y = droid.button_a_was_pressed()
 	txt = "app version is: " + str(x) + " button A was pressed: " + str(y)
 	droid.makeToast(txt)
