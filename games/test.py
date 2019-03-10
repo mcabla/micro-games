e48a1e27d5d0bef768756b2a7a6ea53cd398d023
Casper H <minecabla@users.noreply.github.com>
2019-03-10 18:19:28 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -7,6 +7,6 @@ time.sleep(5)
 
 while 1:
 	y = droid.button_a_was_pressed()
-	txt = "app version is: " + str(x) + " button A was pressed: " + str(y)
+	txt = "Button A was pressed: " + str(y)
 	droid.makeToast(txt)
 	time.sleep(5)
