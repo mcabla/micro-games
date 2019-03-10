299a90e2aca5902928dfc096946605345eca50d0
Casper H <minecabla@users.noreply.github.com>
2019-03-11 00:04:03 +0100

    Update coinflip.py

--- games/coinflip.py
+++ games/coinflip.py
@@ -4,21 +4,16 @@ import random, time
 random.seed()
 
 display.scroll("Kop=A, Munt=B")
-
-droid.makeToast("Coin Flip")
-time.sleep(11)
 keuze = ""
 
 while True:
     if button_a.is_pressed():
         keuze = "kop"
-        droid.makeToast("TEST: A is ingedrukt!")
         break
  
 
     if button_b.is_pressed():
         keuze = "munt"
-        droid.makeToast("TEST: b is ingedrukt!")
         break
 
     
