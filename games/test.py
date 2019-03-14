51dba973c452231f18f467b509d2d98fb53c2b40
Casper H <minecabla@users.noreply.github.com>
2019-03-14 11:10:19 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -9,7 +9,9 @@ keuze = ""
 while True:
     if button_a.is_pressed():
         display.set_pixel(1,1,9)
+        display.set_pixel(1,2,0)
  
 
     if button_b.is_pressed():
         display.set_pixel(1,2,9)
+        display.set_pixel(1,1,0)
