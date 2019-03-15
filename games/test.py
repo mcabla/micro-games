a714cf320ad052d56728d61c1d614de1a73a7865
Casper H <minecabla@users.noreply.github.com>
2019-03-16 00:34:26 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -2,7 +2,7 @@ from microbit import *
 import random
 
 while True:
-    if button_a.is_pressed():
+    if button_a.was_pressed():
         blad = Image("00000:"
              "09990:"
              "09990:"
@@ -10,5 +10,6 @@ while True:
              "09990:")
         display.show(blad)
  
-    if button_b.is_pressed():
+    if button_b.was_pressed():
         display.set_pixel(1,1,0)
+    sleep(30)
