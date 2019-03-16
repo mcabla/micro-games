74080724477779abb4e6d44d85a3d76673a9b827
Casper H <minecabla@users.noreply.github.com>
2019-03-16 15:14:41 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -28,14 +28,37 @@ keuze2 = [blad, steen, schaar]
 i = 0
 while True:
     if button_a.was_pressed():
-        i -= 1
-        if i == -1:
-            i = 2
-        display.show(keuze2[i])
- 
-    elif button_b.was_pressed():
         i += 1
         if i == 3:
             i = 0
         display.show(keuze2[i])
+ 
+    elif button_b.was_pressed():
+        break
     time.sleep(0.2)
+    
+i2 = random.randint(0,2)
+
+score = 0
+if i == i2:
+  score = 1
+  display.scroll('gelijkspel')
+else:
+    if i2==0:
+        if i == 2:
+            score = 2
+            display.scroll("gewonnen")
+        elif i == 1:
+            display.scroll("verloren")
+    elif i2 == 1:
+        if i == 0:
+            score = 2
+            display.scroll("gewonnen")
+        elif i == 2:
+            display.scroll("verloren")
+    elif keuze1 == 2:
+        if huidige == 0:
+            display.scroll("verloren")
+        elif huidige == 1:
+            score = 2
+            display.scroll("gewonnen")
