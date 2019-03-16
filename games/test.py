7d053ed4cfc7ee326f083e8dd40b275b7c2b2abe
Casper H <minecabla@users.noreply.github.com>
2019-03-16 14:37:03 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -2,15 +2,40 @@ from microbit import *
 import random
 import time
 
-while True:
-    if button_a.was_pressed():
-        blad = Image("00000:"
+#afbeeldingen
+blad = Image("00000:"
              "09990:"
              "09990:"
              "09990:"
              "09990:")
-        display.show(blad)
+
+
+steen = Image("00000:"
+              "00000:"
+              "09990:"
+              "99999:"
+              "99999:")
+
+
+schaar = Image("99009:"
+               "99090:"
+               "00900:"
+               "99090:"
+               "99009:")
+
+keuze2 = [blad, steen, schaar]
+
+i = 0
+while True:
+    if button_a.was_pressed():
+        i -= 1
+        if i == -1:
+            i = 3
+        display.show(keuze[i])
  
     elif button_b.was_pressed():
-        display.set_pixel(1,1,0)
+        i += 1
+        if i == 3:
+            i = 0
+        display.show(keuze[i])
     time.sleep(0.2)
