c31918ab2fd9d21aab76cd49ff9459c8bca28715
Casper H <minecabla@users.noreply.github.com>
2019-03-28 09:01:32 +0100

    Update test.py

--- games/test.py
+++ games/test.py
@@ -1,68 +1,59 @@
+#op knopjes drukken en tellen
+#winnaar heeft kortste tijd (variabele verschil)
+#winnaar heeft hoogste score (variabele score = 1/verschil)
+
 from microbit import *
-import random
 import time
-
-random.seed()
-
-#afbeeldingen
-blad = Image("00000:"
-             "09990:"
-             "09990:"
-             "09990:"
-             "09990:")
-
-
-steen = Image("00000:"
-              "00000:"
-              "09990:"
-              "99999:"
+display.clear()
+img1 =  Image("99999:"
+              "95559:"
+              "95059:"
+              "95559:"
               "99999:")
 
+img2 =  Image("55555:"
+              "50005:"
+              "50905:"
+              "50005:"
+              "55555:")
 
-schaar = Image("99009:"
-               "99090:"
-               "00900:"
-               "99090:"
-               "99009:")
+img3 =  Image("00000:"
+              "09990:"
+              "09590:"
+              "09990:"
+              "00000:")
+imgs = [img1,img2,img3]
+aantal=0
+tijd = True
+start = running_time()+1000000
+eind = 0
+verschil = 0
 
-keuze2 = [blad, steen, schaar]
-
-display.show(blad)
-
-i = 0
-while True:
-    if button_a.was_pressed():
-        i += 1
-        if i == 3:
-            i = 0
-        display.show(keuze2[i])
- 
-    elif button_b.was_pressed():
+while tijd:
+  time.sleep(0.01)
+  if button_a.was_pressed():
+    start = min(start, running_time())
+    aantal +=1
+    if aantal >= 10:  
+      if aantal >=50:
+        eind = running_time()
+        verschil = (eind - start)/1000
+        display.show(imgs)
+        display.show(imgs)
+        display.scroll(str(verschil))
+        time.sleep(1)
+        score = 1/verschil
         break
-    time.sleep(0.2)
-    
-i2 = random.randint(0,2)
-
-score = 0
-if i == i2:
-  score = 1
-  display.scroll('gelijkspel')
-else:
-    if i2==0:
-        if i == 2:
-            score = 2
-            display.scroll("gewonnen")
-        elif i == 1:
-            display.scroll("verloren")
-    elif i2 == 1:
-        if i == 0:
-            score = 2
-            display.scroll("gewonnen")
-        elif i == 2:
-            display.scroll("verloren")
-    elif keuze1 == 2:
-        if huidige == 0:
-            display.scroll("verloren")
-        elif huidige == 1:
-            score = 2
-            display.scroll("gewonnen")
+      stra = str(aantal)
+      y=int(stra[0])
+      if stra[1]=='0':
+        x=5
+        y-=1
+      else:
+        x=int(stra[1])/2
+      
+      if aantal % 2 == 0:
+        display.set_pixel(x-1, y, 9)
+    elif aantal < 10:
+      if aantal % 2 == 0:
+        display.set_pixel(aantal/2-1, 0, 9)
