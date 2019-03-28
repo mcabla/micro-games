6e15e10e43c7fff6090a987d5e5f8e7140006b02
Tesax123 <48088982+Tesax123@users.noreply.github.com>
2019-03-28 11:06:48 +0100

    Vallende appels
    links rechts bewegen om te vangen

---
+++ games/VallendeAppels.py
@@ -0,0 +1,105 @@
+#'appels' vallen steeds sneller naar beneden
+# variabele score is gevangen appels
+# aftellen in het begin
+
+from microbit import *
+import time
+import random
+
+
+bezig = True
+appel_aanwezig = False
+score = 0 
+tics = 11
+
+
+
+def linksrechts():
+  global x
+  if button_a.was_pressed():
+    if x>0:
+      display.set_pixel(x,4,0)
+      x-=1
+      display.set_pixel(x,4,9)
+  elif button_b.was_pressed():
+    if x<4:
+      display.set_pixel(x,4,0)
+      x+=1
+      display.set_pixel(x,4,9)
+
+  time.sleep(0.05)
+    
+
+tijdval=1
+def nieuwe_appel():
+  global appel_aanwezig
+  global xa
+  global ya
+  appel_aanwezig=True
+  ya=0
+  xa = random.randint(0,4)
+  display.set_pixel(xa,ya,6)
+  
+  
+def appel_valt():
+  global appel_aanwezig
+  global xa
+  global ya
+  i=1
+  while ya<4:
+    linksrechts()
+    if i%tics==0:
+      if ya<3:
+        linksrechts()
+        display.set_pixel(xa,ya,0)
+        ya+=1
+        display.set_pixel(xa,ya,6)
+      else:  
+        linksrechts()
+        appel_aanwezig=False
+        break
+    i+=1
+
+  display.set_pixel(xa,ya,0)
+  appel_aanwezig=False
+
+def check():
+  global score
+  global xa
+  global ya
+  global x
+  global bezig 
+  
+  if x==xa:
+    score+=1
+    pass
+  else:
+    bezig = False
+  
+
+bezigint=0
+
+display.show('3')
+time.sleep(1)
+display.show('2')
+time.sleep(1)
+display.show('1')
+time.sleep(1)
+display.show(Image("00000:00000:00000:00000:00000"))
+x=2
+display.set_pixel(x,4,9)
+
+while bezig:
+  linksrechts()
+  if not appel_aanwezig:
+    nieuwe_appel()
+  appel_valt()
+  check()
+  if tics>1 and bezigint%2==0:
+    tics-=1
+  bezigint+=1
+
+display.scroll(score)
+
+
+  
