f2f70166aa492e29f5e74eb465c1b22ec777f0ce
lantson <47846904+lantson@users.noreply.github.com>
2019-03-31 16:31:59 +0200

    Update hogerlager.py

--- games/hogerlager.py
+++ games/hogerlager.py
@@ -1,52 +1,52 @@
-from microbit import 
+from microbit import *
 import random
 import time
 
 
 score = 0
 
-for i in range(0,4)
+for i in range(0,4):
     time.sleep(1)
     getal = random.randrange(0,9)
-    # display.scroll(Kies A voor Lager, B voor Hoger)
+    # display.scroll("Kies A voor Lager, B voor Hoger")
 
     willekeurig = random.randrange(0,9)
     display.show(str(willekeurig))
     
     
 
-    while True
+    while True:
 
-      if button_a.is_pressed() and int(willekeurig)  int(getal)
-
-        display.clear()
-        score += 1
-        display.scroll(JUIST!)
-        display.scroll(int(getal))
-        break
-        
-      elif button_a.is_pressed() and int(willekeurig)  int(getal) 
-        display.scroll(FOUT!)
-        display.scroll(int(getal))
-        break
-        
-        
-      if button_b.is_pressed() and int(willekeurig)  int(getal)
+      if button_a.is_pressed() and int(willekeurig) > int(getal):
 
         display.clear()
         score += 1
-        display.scroll(JUIST!)
+        display.scroll("JUIST!")
         display.scroll(int(getal))
         break
-
-      elif button_b.is_pressed() and int(willekeurig)  int(getal)
+        
+      elif button_a.is_pressed() and int(willekeurig) < int(getal) :
+        display.scroll("FOUT!")
+        display.scroll(int(getal))
+        break
+        
+        
+      if button_b.is_pressed() and int(willekeurig) < int(getal):
 
         display.clear()
-        display.scroll(FOUT!)
+        score += 1
+        display.scroll("JUIST!")
         display.scroll(int(getal))
         break
 
-display.scroll(SCORE)
+      elif button_b.is_pressed() and int(willekeurig) > int(getal):
+
+        display.clear()
+        display.scroll("FOUT!")
+        display.scroll(int(getal))
+        break
+
+display.scroll("SCORE:")
 display.show(str(score))
                
               
