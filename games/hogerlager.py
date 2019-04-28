b55a4826d1c4d6ce1e044d604fc527964fd60ba1
Casper H <minecabla@users.noreply.github.com>
2019-04-28 23:09:48 +0200

    Update hogerlager.py

--- games/hogerlager.py
+++ games/hogerlager.py
@@ -4,48 +4,45 @@ import time
 
 
 score = 0
+random.seed()
 
 for i in range(0,4):
-    time.sleep(1)
-    getal = random.randrange(0,9)
-    # display.scroll("Kies A voor Lager, B voor Hoger")
+	time.sleep(1)
+    	getal = random.randrange(0,9)
+    	# display.scroll("Kies A voor Lager, B voor Hoger")
 
-    willekeurig = random.randrange(0,9)
-    willekeurig_str = str(willekeurig) #Wegens een beperking in de python implementatie op Android, moeten we helaas onze strings maken buiten de parameters van de microbit functies
-    display.show(willekeurig_str)
+    	willekeurig = random.randrange(1,8)
+	willekeurig_str = str(willekeurig) #Wegens een beperking in de python implementatie op Android, moeten we helaas onze strings maken buiten de parameters van de microbit functies
+    	display.show(willekeurig_str)
     
-    
-    while True:
+	while True:
+		time.sleep(0.2) #We zetten een niet merkbare vertraging op de lus om te voorkomen dat er niet te veel requests naar de microbit gestuurd worden.
+		if button_a.was_pressed():
+			if willekeurig > getal:
+				score += 1
+				str = 'JUIST!' + str(getal)
+				display.scroll(str)
+				time.sleep(6) #Wegens een beperking in de python implementatie op Android, moeten we handmatig een sleep toevoegen achter een scroll
+				break
+			else:
+				str = 'FOUT!' + str(getal)
+				display.scroll(str)
+				time.sleep(6)
+				break
+		elif button_b.was_pressed():
+			if willekeurig < getal:
+				score += 1
+				str = 'JUIST!' + str(getal)
+				display.scroll(str)
+				time.sleep(6)
+				break
+			else:
+				str = 'FOUT!' + str(getal)
+				display.scroll(str)
+				time.sleep(6)
+				break
 
-	if button_a.was_pressed()
-		if willekeurig > getal:
-			score += 1
-			str = 'JUIST! ' + str(getal)
-			display.scroll(str)
-			time.sleep(3) #Wegens een beperking in de python implementatie op Android, moeten we handmatig een sleep toevoegen achter een scroll
-			break
-		else:
-			str = 'FOUT! ' + str(getal)
-			display.scroll(str)
-			time.sleep(3)
-			break
-
-        
-	elif button_b.was_pressed()
-		if willekeurig < getal:
-			score += 1
-			str = 'JUIST! ' + str(getal)
-			display.scroll(str)
-			time.sleep(3)
-			break
-		else:
-			str = 'FOUT! ' + str(getal)
-			display.scroll(str)
-			time.sleep(3)
-			break
-		
-	time.sleep(0.05) #We zetten een niet merkbare vertraging op de lus om te voorkomen dat er niet te veel requests naar de microbit gestuurd worden.
-
-str = 'SCORE: ' + str(score)
+str = 'SCORE=' + str(score)
 display.scroll(str)
-time.sleep(4)
+time.sleep(7)
+               
