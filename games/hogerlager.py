9cca462c239a09ff5df5019c2d6f51e0e04d154d
Casper H <minecabla@users.noreply.github.com>
2019-04-28 13:26:13 +0200

    Update hogerlager.py

--- games/hogerlager.py
+++ games/hogerlager.py
@@ -11,41 +11,40 @@ for i in range(0,4):
     # display.scroll("Kies A voor Lager, B voor Hoger")
 
     willekeurig = random.randrange(0,9)
-	willekeurig_str = str(willekeurig) #Wegens een beperking in de python implementatie op Android, moeten we helaas onze strings maken buiten de parameters van de microbit functies
+    willekeurig_str = str(willekeurig) #Wegens een beperking in de python implementatie op Android, moeten we helaas onze strings maken buiten de parameters van de microbit functies
     display.show(willekeurig_str)
     
     
-
     while True:
 
-		if button_a.was_pressed()
-			if willekeurig > getal:
-				score += 1
-				str = 'JUIST! ' + str(getal)
-				display.scroll(str)
-				time.sleep(3) #Wegens een beperking in de python implementatie op Android, moeten we handmatig een sleep toevoegen achter een scroll
-				break
-			else:
-				str = 'FOUT! ' + str(getal)
-				display.scroll(str)
-				time.sleep(3)
-				break
+	if button_a.was_pressed()
+		if willekeurig > getal:
+			score += 1
+			str = 'JUIST! ' + str(getal)
+			display.scroll(str)
+			time.sleep(3) #Wegens een beperking in de python implementatie op Android, moeten we handmatig een sleep toevoegen achter een scroll
+			break
+		else:
+			str = 'FOUT! ' + str(getal)
+			display.scroll(str)
+			time.sleep(3)
+			break
+
         
-        
-		if button_b.was_pressed()
-			if willekeurig < getal:
-				score += 1
-				str = 'JUIST! ' + str(getal)
-				display.scroll(str)
-				time.sleep(3)
-				break
-			else:
-				str = 'FOUT! ' + str(getal)
-				display.scroll(str)
-				time.sleep(3)
-				break
+	elif button_b.was_pressed()
+		if willekeurig < getal:
+			score += 1
+			str = 'JUIST! ' + str(getal)
+			display.scroll(str)
+			time.sleep(3)
+			break
+		else:
+			str = 'FOUT! ' + str(getal)
+			display.scroll(str)
+			time.sleep(3)
+			break
 		
-		time.sleep(0.05) #We zetten een niet merkbare vertraging op de lus om te voorkomen dat er niet te veel requests naar de microbit gestuurd worden.
+	time.sleep(0.05) #We zetten een niet merkbare vertraging op de lus om te voorkomen dat er niet te veel requests naar de microbit gestuurd worden.
 
 str = 'SCORE: ' + str(score)
 display.scroll(str)
