bef48f7fe51f9bddc406207fd1673a61f1cacf81
Casper H <minecabla@users.noreply.github.com>
2019-04-28 13:23:18 +0200

    Update hogerlager.py

--- games/hogerlager.py
+++ games/hogerlager.py
@@ -5,38 +5,48 @@ import time
 
 score = 0
 
-run = True
-while run:
-    getal = random.randrange(0,9) # willekeurig getal dat op het scherm komt
-    willekeurig = random.randrange(0,9) # het getal naar waar gegokt moet worden
-    getal_str = str(getal)
-    display.show(getal_str)
+for i in range(0,4):
+    time.sleep(1)
+    getal = random.randrange(0,9)
+    # display.scroll("Kies A voor Lager, B voor Hoger")
+
+    willekeurig = random.randrange(0,9)
+	willekeurig_str = str(willekeurig) #Wegens een beperking in de python implementatie op Android, moeten we helaas onze strings maken buiten de parameters van de microbit functies
+    display.show(willekeurig_str)
+    
+    
+
     while True:
 
-        if button_a.was_pressed(): # Knop A: het te zoeken getal is LAGER dan het getal dat op het scherm staat
-            if int(willekeurig) >= int(getal):
-                score += 1
-                display.scroll("JUIST!")
-                #time.sleep(3)        
-            else:
-                display.scroll("FOUT!")
-                #time.sleep(3)
-                run = False
-            break
+		if button_a.was_pressed()
+			if willekeurig > getal:
+				score += 1
+				str = 'JUIST! ' + str(getal)
+				display.scroll(str)
+				time.sleep(3) #Wegens een beperking in de python implementatie op Android, moeten we handmatig een sleep toevoegen achter een scroll
+				break
+			else:
+				str = 'FOUT! ' + str(getal)
+				display.scroll(str)
+				time.sleep(3)
+				break
         
         
-        elif button_b.was_pressed(): # Knop B: het te zoeken getal is HOGER dan het getal dat op het scherm staat
-            if int(willekeurig) <= int(getal):
-                score += 1
-                display.scroll("JUIST!")
-                #time.sleep(3)
-            else:
-                display.scroll("FOUT!")
-                #time.sleep(3)
-                run = False
-            break
-        
-        time.sleep(0.02)
+		if button_b.was_pressed()
+			if willekeurig < getal:
+				score += 1
+				str = 'JUIST! ' + str(getal)
+				display.scroll(str)
+				time.sleep(3)
+				break
+			else:
+				str = 'FOUT! ' + str(getal)
+				display.scroll(str)
+				time.sleep(3)
+				break
+		
+		time.sleep(0.05) #We zetten een niet merkbare vertraging op de lus om te voorkomen dat er niet te veel requests naar de microbit gestuurd worden.
 
-display.scroll("SCORE: " + str(score))
+str = 'SCORE: ' + str(score)
+display.scroll(str)
 time.sleep(4)
