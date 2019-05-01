5fbeec7f540e76902ad0f99e494edf148c201fe9
lowchris3 <48088700+lowchris3@users.noreply.github.com>
2019-05-01 12:31:25 +0200

    Update coinflip.py

--- games/coinflip.py
+++ games/coinflip.py
@@ -17,7 +17,42 @@ while True:
         keuze = "munt"
         break
 
-    
+#worp
+fase1 = Image("00000:"
+              "00000:"
+              "00000:"
+              "00000:"
+              "09990:")
+display.show(fase1)
+time.sleep(0.3)
+ 
+fase2 = Image("00000:"
+              "00090:"
+              "00900:"
+              "09000:"
+              "00000:")
+display.show(fase2)
+time.sleep(0.3)
+
+fase3 = Image("00900:"
+              "00900:"
+              "00900:"
+              "00000:"
+              "00000:")
+display.show(fase3)
+time.sleep(0.3)
+
+fase4 = Image("00000:"
+              "09000:"
+              "00900:"
+              "00090:"
+              "00000:")
+display.show(fase4)
+time.sleep(0.3)
+display.show(fase1)
+time.sleep(0.3)
+
+#---    
 x= random.randint(0,1)
 if x==1:
     x="kop"
